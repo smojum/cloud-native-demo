9a89b858acd21d17b280aed8e91d187905d49b82
smojumd <santanu.mojumder@landsend.com>
2019-07-22 15:38:56 -0500

    addition

--- demo-service/src/main/java/com/landsend/demoservice/config/ConfigDemoController.java
+++ demo-service/src/main/java/com/landsend/demoservice/config/ConfigDemoController.java
@@ -2,11 +2,13 @@ package com.landsend.demoservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigDemoController {
 
     @Autowired
